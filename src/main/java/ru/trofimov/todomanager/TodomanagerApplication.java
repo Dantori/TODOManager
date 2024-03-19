df6896c5305b750ee2f5138fd19db0b0b81d7965
Trofimov Ilya <guardianshield.cpp@gmail.com>
2024-03-19 20:25:39 +0300

    Создана структура пакетов, добавлены зависимости

---
+++ src/main/java/ru/trofimov/todomanager/TodomanagerApplication.java
@@ -0,0 +1,13 @@
+package ru.trofimov.todomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodomanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodomanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/trofimov/todomanager/TodomanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.trofimov.todomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodomanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
