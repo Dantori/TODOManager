4deed0662a811ca7792ddc5e719a06dc5782c033
Trofimov Ilya <guardianshield.cpp@gmail.com>
2024-03-19 20:43:16 +0300

    Создан контроллер задач

---
+++ src/main/java/ru/trofimov/todomanager/web/controller/TodoController.java
@@ -0,0 +1,53 @@
+package ru.trofimov.todomanager.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.trofimov.todomanager.domain.account.User;
+import ru.trofimov.todomanager.domain.todo.Todo;
+import ru.trofimov.todomanager.service.TodoService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/todos")
+public class TodoController {
+
+    private final TodoService todoService;
+
+    @Autowired
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping
+    public String getTodoList(Model model, @AuthenticationPrincipal User user) {
+        List<Todo> todos = todoService.getAllByUserId(user.getId());
+        model.addAttribute("todos", todos);
+        model.addAttribute("newTodo", new Todo());
+        return "todo";
+    }
+
+    @PostMapping("/add")
+    public String addTodo(@ModelAttribute("newTodo") Todo newTodo, @AuthenticationPrincipal User user) {
+        newTodo.setUser(user);
+        todoService.addTodo(newTodo);
+        return "redirect:/todos";
+    }
+
+    @PostMapping("/{id}/toggle")
+    public String toggleTodoStatus(@PathVariable("id") Long id) {
+        Todo todo = todoService.getById(id);
+        todo.setCompleted(!todo.isCompleted());
+        todoService.addTodo(todo);
+        return "redirect:/todos";
+    }
+
+    @PostMapping("/{id}/delete")
+    public String deleteTodoById(@PathVariable Long id) {
+        todoService.deleteTodoById(id);
+        return "redirect:/todos";
+    }
+}
